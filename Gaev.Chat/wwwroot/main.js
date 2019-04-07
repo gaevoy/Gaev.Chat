97f3384d0c4dc6e7c0ba6fb5e04db362b95eb423
Vladimir Gaevoy <vova.gaevoy@gmail.com>
2019-04-07 16:09:19 +0200

    End-to-end Encrypted Chat

--- Gaev.Chat/wwwroot/main.js
+++ Gaev.Chat/wwwroot/main.js
@@ -13,27 +13,47 @@ function enterChat(onDone) {
         room = roomInput.value;
         nickname = nicknameInput.value;
         if (room.length && nickname.length) {
-            document.querySelector('.lobby').classList.add('hidden');
-            window.history.pushState(null, null, '#room/' + window.encodeURI(room));
-            onDone(room, nickname);
+            document.querySelector('.lobby .join-form').innerText = 'Generating cryptographic key...';
+            PgpKey.generate(nickname, myKey => {
+                document.querySelector('.lobby').classList.add('hidden');
+                window.history.pushState(null, null, '#room/' + window.encodeURI(room));
+                onDone(room, nickname, myKey);
+            });
         }
     });
 }
 
-function joinChat(room, nickname) {
+function joinChat(room, nickname, myKey) {
     let logs = document.querySelector(".chat .logs");
     let newMessage = document.querySelector('.chat .new-message');
     let chatApi = new ChatApi();
+    let keys = {};
     document.querySelector('.chat .new-message-form').addEventListener('submit', evt => {
         evt.preventDefault();
-        chatApi.sendMessage(room, {sender: nickname, text: newMessage.value});
+        Object.keys(keys).forEach(keyId =>
+            keys[keyId].encrypt(newMessage.value, cipher =>
+                chatApi.sendMessage(room, {sender: myKey.id(), recipient: keyId, text: cipher})));
         newMessage.value = '';
     });
     chatApi.listenToMessages(room,
         message => {
-            renderMessage(message.sender, message.text);
+            let sender = keys[message.sender];
+            let recipient = message.recipient && keys[message.recipient];
+            if (message.publicKey) {
+                if (sender) return;
+                PgpKey.load(message.publicKey, key => {
+                    let isMyKey = myKey.id() === key.id();
+                    keys[key.id()] = isMyKey ? myKey : key;
+                    renderMessage(key.nickname() + ' joined', '');
+                    if (!isMyKey)
+                        chatApi.sendMessage(room, {sender: myKey.id(), publicKey: myKey.public()});
+                });
+            } else if (sender && recipient && recipient.canDecrypt()) {
+                recipient.decrypt(message.text, text =>
+                    renderMessage(sender.nickname(), text));
+            }
         },
-        () => chatApi.sendMessage(room, {sender: nickname + ' joined', text: '',}));
+        () => chatApi.sendMessage(room, {sender: myKey.id(), publicKey: myKey.public()}));
     document.querySelector('.chat').classList.remove('hidden');
     newMessage.select();
 
@@ -69,5 +89,54 @@ class ChatApi {
         req.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
         req.send(JSON.stringify(JSON.stringify(message)));
         console.log('Message sent', message);
+    }
+}
+
+class PgpKey {
+    constructor(key) {
+        this._key = key;
+        if (this.canDecrypt()) {
+            this._ring = new kbpgp.keyring.KeyRing();
+            this._ring.add_key_manager(key);
+        }
+    }
+
+    public() {
+        return this._key.armored_pgp_public;
+    }
+
+    id() {
+        return this._key.get_pgp_short_key_id();
+    }
+
+    nickname() {
+        return this._key.userids[0].get_username();
+    }
+
+    encrypt(text, onDone) {
+        kbpgp.box({msg: text, encrypt_for: this._key}, (_, cipher) =>
+            onDone(cipher));
+    }
+
+    canDecrypt() {
+        return this._key.can_decrypt();
+    }
+
+    decrypt(cipher, onDone) {
+        kbpgp.unbox({keyfetch: this._ring, armored: cipher, progress_hook: null}, (_, literals) =>
+            onDone(literals[0].toString()));
+    }
+
+    static generate(nickname, onDone) {
+        let opt = {userid: nickname, primary: {nbits: 1024}, subkeys: []};
+        kbpgp.KeyManager.generate(opt, (_, key) =>
+            key.sign({}, () =>
+                key.export_pgp_public({}, () =>
+                    onDone(new PgpKey(key)))));
+    }
+
+    static load(publicKey, onDone) {
+        kbpgp.KeyManager.import_from_armored_pgp({armored: publicKey}, (_, key) =>
+            onDone(new PgpKey(key)));
     }
 }
